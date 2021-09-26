478853b504c6c8bde345a5ee9e82fd6519f75769
MissaDavid <mei.chohee@gmail.com>
2021-09-26 15:18:34 +1300

    Add the markdown loader to the webpack config

--- next.config.js
+++ next.config.js
@@ -2,4 +2,12 @@
 module.exports = {
     reactStrictMode: true,
     target: 'serverless',
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.md$/,
+            loader: 'frontmatter-markdown-loader',
+            options: { mode: ['react-component'] },
+        })
+        return config
+    },
 }
